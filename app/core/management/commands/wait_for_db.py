e8e2e340a0141044aa3783e87526dd664c5e603c
solomonT <tseesolo@gmail.com>
2025-01-01 08:47:53 +0100

    Added APIDocumentation

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,10 +3,9 @@ Django command to wait for the database to be available
 """
 import time
 
-from psycopg2 import OperationalError as Psycopg20pError
-
-from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg20pError
 
 
 class Command(BaseCommand):
--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,12 @@
 """
 Database Model
 """
-from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin,)
+    PermissionsMixin, )
+from django.db import models
+
 
 class UserManager(BaseUserManager):
     """Manager for user"""
